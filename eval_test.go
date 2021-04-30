704d725fc4eae4bdba3fafbf56787f06f2444a4b
zhuyongsheng <yongsheng.zhu@shopee.com>
2021-04-30 16:51:21 +0800

    add benchmark

--- eval_test.go
+++ eval_test.go
@@ -3,6 +3,7 @@ package goeval
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestExecute(t *testing.T) {
@@ -13,4 +14,32 @@ func TestExecute(t *testing.T) {
 			count=count+i
 		}`))
 	t.Log(s.Eval(`print(count)`))
+}
+
+func TestNewScope(t *testing.T) {
+	s := NewScope()
+	//t.Log(s.Eval("\"ab\" + \"cd\""))
+	for i := 0; i < 100; i++ {
+		go func(n int) {
+			v, e := s.Eval(fmt.Sprintf(`2 + %d`, n))
+			if e != nil {
+				panic(e)
+			}
+			fmt.Println(v)
+		}(i)
+	}
+	time.Sleep(1 * time.Second)
+}
+
+func BenchmarkScope_Eval(b *testing.B) {
+
+	s := NewScope()
+	for i := 0; i < b.N; i++ {
+
+		v, e := s.Eval("\"ab\" + \"cd\"")
+		if e != nil {
+			panic(e)
+		}
+		_ = v
+	}
 }
