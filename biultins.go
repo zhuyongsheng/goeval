14be52268e973960df20d9187412d204850c61ea
zhuyongsheng <37647564+zhuyongsheng@users.noreply.github.com>
2021-05-07 22:50:10 +0800

    move builtins

--- biultins.go
+++ biultins.go
@@ -6,6 +6,40 @@ import (
 	"reflect"
 )
 
+var (
+	builtins = map[string]interface{}{
+		"nil":    nil,
+		"true":   true,
+		"false":  false,
+		"append": Append,
+		"make":   Make,
+		"len":    Len,
+	}
+	builtinTypes = map[string]reflect.Type{
+		"bool":       reflect.TypeOf(true),
+		"byte":       reflect.TypeOf(byte(0)),
+		"rune":       reflect.TypeOf(rune(0)),
+		"string":     reflect.TypeOf(string(0)),
+		"int":        reflect.TypeOf(0),
+		"int8":       reflect.TypeOf(int8(0)),
+		"int16":      reflect.TypeOf(int16(0)),
+		"int32":      reflect.TypeOf(int32(0)),
+		"int64":      reflect.TypeOf(int64(0)),
+		"uint":       reflect.TypeOf(uint(0)),
+		"uint8":      reflect.TypeOf(uint8(0)),
+		"uint16":     reflect.TypeOf(uint16(0)),
+		"uint32":     reflect.TypeOf(uint32(0)),
+		"uint64":     reflect.TypeOf(uint64(0)),
+		"uintptr":    reflect.TypeOf(uintptr(0)),
+		"float32":    reflect.TypeOf(float32(0)),
+		"float64":    reflect.TypeOf(float64(0)),
+		"complex64":  reflect.TypeOf(complex64(0)),
+		"complex128": reflect.TypeOf(complex128(0)),
+		"error":      reflect.TypeOf(errors.New("")),
+	}
+)
+
+
 // Append is a runtime replacement for the append function
 func Append(arr interface{}, elements ...interface{}) (interface{}, error) {
 	arrVal := reflect.ValueOf(arr)
