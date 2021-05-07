6b80a22ec7bd28071a774c4f4b065c0dd46c655a
zhuyongsheng <yongsheng.zhu@shopee.com>
2021-05-07 20:10:10 +0800

    add map make support

--- helper.go
+++ helper.go
@@ -20,7 +20,7 @@ func Append(arr interface{}, elements ...interface{}) (interface{}, error) {
 }
 
 // Make is a runtime replacement for the make function
-func Make(t interface{}, args ...interface{}) (interface{}, error) {
+func Make(t interface{}, args ...interface{}) (v interface{}, err error) {
 	typ, isType := t.(reflect.Type)
 	if !isType {
 		return nil, fmt.Errorf("invalid type %#v", t)
@@ -28,44 +28,55 @@ func Make(t interface{}, args ...interface{}) (interface{}, error) {
 	switch typ.Kind() {
 	case reflect.Slice:
 		if len(args) < 1 || len(args) > 2 {
-			return nil, errors.New("invalid number of arguments, missing len or extra")
+			return nil, errors.New("invalid number of arguments for make slice, 1 or 2 needed")
 		}
-		length, isInt := args[0].(int)
-		if !isInt {
-			return nil, errors.New("len is not int")
+		length, err := getInteger(args[0])
+		if err != nil {
+			return nil, err
 		}
 		capacity := length
 		if len(args) == 2 {
-			capacity, isInt = args[0].(int)
-			if !isInt {
-				return nil, errors.New("len is not int")
+			capacity, err = getInteger(args[1])
+			if err != nil {
+				return nil, err
 			}
 		}
-		slice := reflect.MakeSlice(typ, length, capacity)
-		return slice.Interface(), nil
+		return reflect.MakeSlice(typ, length, capacity).Interface(), nil
 
 	case reflect.Chan:
 		if len(args) > 1 {
-			fmt.Printf("CHAN ARGS %#v", args)
 			return nil, errors.New("too many arguments")
 		}
 		size := 0
 		if len(args) == 1 {
-			var isInt bool
-			size, isInt = args[0].(int)
-			if !isInt {
-				return nil, errors.New("size is not int")
+			size, err = getInteger(args[0])
+			if err != nil {
+				return nil, err
 			}
 		}
-		buffer := reflect.MakeChan(typ, size)
-		return buffer.Interface(), nil
-
+		return reflect.MakeChan(typ, size).Interface(), nil
+	case reflect.Map:
+		size := 0
+		if len(args) > 0 {
+			size, err = getInteger(args[0])
+			if err != nil {
+				return nil, err
+			}
+		}
+		return reflect.MakeMapWithSize(typ, size).Interface(), nil
 	default:
-		return nil, fmt.Errorf("unknown kind type %T", t)
+		return nil, fmt.Errorf("make unsupported type %T", t)
 	}
 }
 
 // Len is a runtime replacement for the len function
 func Len(t interface{}) (interface{}, error) {
 	return reflect.ValueOf(t).Len(), nil
+}
+
+func getInteger(arg interface{}) (int, error) {
+	if i, ok := arg.(int); ok {
+		return i, nil
+	}
+	return 0, errors.New("error not int")
 }
